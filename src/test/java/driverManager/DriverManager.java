121885e255e4e60fbbc3d138be471474fb75a1e3
Carlos Naveda <navedacarlosm@gmail.com>
2026-03-04 10:32:55 -0500

    DriverManager with Chrome Driver

--- src/test/java/driverManager/DriverManager.java
+++ src/test/java/driverManager/DriverManager.java
@@ -1,4 +1,29 @@
 package driverManager;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class DriverManager {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void initDriver(WebDriver driver) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+
 }
