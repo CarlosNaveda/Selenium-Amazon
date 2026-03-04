0c8984b6b2dd055665e5f10a5c9e1ff86af5aae5
Carlos Naveda <navedacarlosm@gmail.com>
2026-03-04 11:10:23 -0500

    create BasePage structure

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,24 @@
 package pages;
 
+import driverManager.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    //Constructor
+    public BasePage() {
+        this.driver = DriverManager.getDriver();
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    //Métodos
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
 }
