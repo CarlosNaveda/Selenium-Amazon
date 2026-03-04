f43082e2b6d5899cee097da773b33faef32216c8
Carlos Naveda <navedacarlosm@gmail.com>
2026-03-04 11:03:55 -0500

    create TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,4 +1,21 @@
 package runner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
+
+
+@CucumberOptions(
+        features = "src/test/resources/features" ,
+        glue = {"steps","hooks"},
+        plugin = {"pretty"},
+        tags = ""
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Test
+    public void runTestNGCucumberTests(){
+
+    }
+
+
 }
